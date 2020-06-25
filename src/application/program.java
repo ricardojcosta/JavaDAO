25f2bb03b78d329bbb595a9e6c1be94315b47e46
ricardojcosta <ricardoredes2006@gmail.com>
2020-06-25 15:17:16 -0300

    Seller class

--- src/application/program.java
+++ src/application/program.java
@@ -1,14 +1,18 @@
 
 package application;
 
+import java.sql.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 
 public class program {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
         
-        System.out.println(obj);
+        Seller vendedor = new Seller(21, "Bob", "bob@gmail.com", new Date(2), 3000.0, obj);
+        
+        System.out.println(vendedor);
     }
     
 }
