6fda6ce297834844d2964f2dcb2c4423a83fa81c
ricardojcosta <ricardoredes2006@gmail.com>
2020-06-25 14:45:10 -0300

    Departament class

---
+++ src/application/program.java
@@ -0,0 +1,14 @@
+
+package application;
+
+import model.entities.Department;
+
+
+public class program {
+    public static void main(String[] args) {
+        Department obj = new Department(1, "books");
+        
+        System.out.println(obj);
+    }
+    
+}
