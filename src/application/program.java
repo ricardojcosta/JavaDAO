b34e0592b9bf12ab2d6e18c0b1c0a255a40fd778
ricardojcosta <ricardoredes2006@gmail.com>
2020-06-26 11:26:22 -0300

    Implementando findById

--- src/application/program.java
+++ src/application/program.java
@@ -2,17 +2,19 @@
 package application;
 
 import java.sql.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 
 public class program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "books");
+                
+        SellerDao vendedorDao = DaoFactory.createSellerDao();
+        Seller seller = vendedorDao.findById(3);
         
-        Seller vendedor = new Seller(21, "Bob", "bob@gmail.com", new Date(2), 3000.0, obj);
-        
-        System.out.println(vendedor);
+        System.out.println(seller);
     }
     
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,33 +1,75 @@
-
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
-
 public class SellerDaoJDBC implements SellerDao {
-
+    
+    private Connection conn;
+    
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+    
     @Override
     public void insert(Seller obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public void update(Seller obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public void deleteById(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public Seller findById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName\n"
+                    + "FROM seller INNER JOIN department\n"
+                    + "ON seller.DepartmentId = department.Id\n"
+                    + "WHERE seller.Id = ?");
+            
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("Depname"));
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setDepartment(dep);
+                return obj;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+            
+        }
+        
     }
-
+    
     @Override
     public List<Seller> findAll() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
