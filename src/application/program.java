6d56a75c6c553dd437e0f32f72a78443ac7fc6c0
ricardojcosta <ricardoredes2006@gmail.com>
2020-08-20 09:10:52 -0300

    todos vendedores

--- src/application/program.java
+++ src/application/program.java
@@ -1,4 +1,3 @@
-
 package application;
 
 import java.util.List;
@@ -7,23 +6,30 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-
 public class program {
+
     public static void main(String[] args) {
-                
+
         SellerDao vendedorDao = DaoFactory.createSellerDao();
-        
+
         System.out.println("==== Teste 1: Vendedor por ID ====");
         Seller seller = vendedorDao.findById(1);
-        
+
         System.out.println(seller);
-        
+
         System.out.println("==== Teste 2: Vendedor por Departamento ====");
         Department dep = new Department(2, null);
         List<Seller> lista = vendedorDao.findByDepartment(dep);
-        for (Seller obj : lista){
+        for (Seller obj : lista) {
+            System.out.println(obj);
+        }
+        
+        System.out.println("==== Teste 3: Todos vendedores ====");
+        lista = vendedorDao.findAll();
+        for (Seller obj : lista) {
             System.out.println(obj);
         }
     }
-    
 }
+
+
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -67,7 +67,39 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId=department.Id "
+                    + "ORDER BY Name");
+
+            rs = st.executeQuery();
+
+            List<Seller> lista = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instanciaDepartamento(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instanciaVendedor(rs, dep);
+                lista.add(obj);
+            }
+            return lista;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+
+        }
+
     }
 
     private Department instanciaDepartamento(ResultSet rs) throws SQLException {
